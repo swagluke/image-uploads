71fca119dfe1ced135eed4d887e8ec583cb6936a
Luke Zhang <zhangl@rose-hulman.edu>
2017-04-25 09:04:52 -0400

    Finished.

--- ImageUploads/src/app/app.component.ts
+++ ImageUploads/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
 import * as firebase from 'firebase';
 
 interface FeaturedPhotoUrls {
   url1?: string;
   url2?: string;
+}
+
+interface Photo {
+  url: string;
 }
 
 @Component({
@@ -15,9 +19,10 @@ interface FeaturedPhotoUrls {
 export class AppComponent {
 
   public featuredPhotoStream: FirebaseObjectObservable<FeaturedPhotoUrls>;
-
+  public photoListStream: FirebaseListObservable<Photo[]>;
   constructor(private af: AngularFire) {
     this.featuredPhotoStream = this.af.database.object("/photos/featured");
+    this.photoListStream = this.af.database.list("/photos/list");
   }
 
   // featuredPhotoSelected(event: MSInputMethodContext) {
@@ -44,6 +49,24 @@ export class AppComponent {
 
   triggerInput1(inputEl) {
     inputEl.click();
-//  document.getElementById("featuredPhoto1Input").click();
+    //  document.getElementById("featuredPhoto1Input").click();
   }
+
+  photoSelectedForList(event: any) {
+    const nextAvailableKey = firebase.database().ref().child(`/photos/list`).push({}).key;
+    const file: File = event.target.files[0]
+    console.log("Selected File", file);
+    const metadata = { "content-type": file.type };
+    const storageRef: firebase.storage.Reference = firebase.storage().ref().child("photos").child("list").child(nextAvailableKey);
+    const uploadTask: firebase.storage.UploadTask = storageRef.put(file, metadata);
+    uploadTask.then((uploadSnapshot: firebase.storage.UploadTaskSnapshot) => {
+      console.log("Upload is complete");
+      const downloadUrl = uploadSnapshot.downloadURL;
+      const newPhoto: Photo = {url: downloadUrl}
+      firebase.database().ref().child(`/photos/list/${nextAvailableKey}`).set(newPhoto);
+      // this.featuredPhotoStream.update({url1: downloadUrl});
+    });
+    console.log("Uploading", file.name);
+  }
+
 }
